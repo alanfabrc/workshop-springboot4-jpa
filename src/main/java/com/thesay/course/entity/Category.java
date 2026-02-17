1d3d3f689904e0e41b2646b6fe0c215ebea6fb8a
Alan Filho <devaneiosdoalan@gmail.com>
2026-02-17 00:52:47 -0300

    Product

--- src/main/java/com/thesay/course/entity/Category.java
+++ src/main/java/com/thesay/course/entity/Category.java
@@ -3,7 +3,9 @@ package com.thesay.course.entity;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 
 @Entity
@@ -16,6 +18,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -39,6 +43,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
